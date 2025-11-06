e081582f8eb15a744893306c1d0e5609459c4914
Inkshaded <griffingreimes@gmail.com>
2025-11-06 02:48:14 -0800

    added option for user to look at menu after going with selected restaurant

--- lunch_decider/engine.py
+++ lunch_decider/engine.py
@@ -109,6 +109,12 @@ def run_cli():
         if again == "y":
             excluded.add(ranked[0][0].name)
             continue
+        elif again == "n" and ranked:
+            show_menu = input("Alright! Glad to have found you a restaurant! Would you like to see the menu? (y/n): ").lower()
+            if show_menu == "y":
+                print("Menu Items:")
+                for mi in ranked[0][0].menu_items:
+                    print(f"{mi}")
         break
 
 def ask_float(prompt, optional=False):
